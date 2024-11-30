7bcf8d19b4c35d73f38efee4c582fb7e2d88d3b0
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-11-30 18:29:34 -0500

    Update Concurrency.c

--- Crust/Concurrency.c
+++ Crust/Concurrency.c
@@ -11,3 +11,17 @@ void execute_parallel(CorrolexTask *tasks, size_t task_count) {
         tasks[i].execute();
     }
 }
+
+void execute_concurrent_tasks(CorrolexInstruction *tasks, size_t task_count) {
+    #pragma omp parallel for
+    for (size_t i = 0; i < task_count; i++) {
+        execute_task(&tasks[i]);
+    }
+}
+
+void execute_parallel(CorrolexTask *tasks, size_t task_count) {
+    #pragma omp parallel for
+    for (size_t i = 0; i < task_count; i++) {
+        tasks[i].execute();
+    }
+}
